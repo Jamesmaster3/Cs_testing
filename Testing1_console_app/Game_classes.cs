97fcc972cf685beebecd907bfa0189f335b40d50
Jamesmaster3 <120321282+Jamesmaster3@users.noreply.github.com>
2024-01-21 22:05:33 +0100

    fix

--- Testing1_console_app/Game_classes.cs
+++ Testing1_console_app/Game_classes.cs
@@ -1,19 +1,5 @@
 using System.Reflection;
 
-public class Treasure : Thing {
-    private double _value;
-    public Treasure(string aName, string aDescription, double aValue): base(aName, aDescription){
-        _value = aValue;
-    }
-    public double Value{
-        get {
-            return _value;
-        }
-        set { _value = value; 
-        }
-    }
-}
-
 public class Thing{
     private string _name;
     private string _description;
@@ -41,5 +27,25 @@ public class Thing{
         set {
             _description = value;
         }        
+    }
+}
+
+public class Treasure : Thing
+{
+    private double _value;
+    public Treasure(string aName, string aDescription, double aValue) : base(aName, aDescription)
+    {
+        _value = aValue;
+    }
+    public double Value
+    {
+        get
+        {
+            return _value;
+        }
+        set
+        {
+            _value = value;
+        }
     }
 }
